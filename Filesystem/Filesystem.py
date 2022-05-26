78d938f9cefb1cae12c6aa75e1e15b4627528586
CybernetHacker14 <ujjwalraut.14@gmail.com>
2022-05-26 21:52:46 +0530

    [WIP] Added more Filesystem functions

--- Filesystem/Filesystem.py
+++ Filesystem/Filesystem.py
@@ -6,6 +6,34 @@ import pathlib
 
 class Filesystem:
     @staticmethod
+    def GetAbsolutePath(path: str) -> str:
+        return os.path.abspath(path)
+
+    @staticmethod
+    def DoesPathExist(path: str) -> bool:
+        return os.path.exists(Filesystem.GetAbsolutePath(path))
+
+    @staticmethod
+    def IsFile(path: str) -> bool:
+        return os.path.isfile(Filesystem.GetAbsolutePath(path))
+
+    @staticmethod
+    def IsFolder(path: str) -> bool:
+        return os.path.isdir(Filesystem.GetAbsolutePath(path))
+
+    @staticmethod
+    def CreateFolder(path: str) -> None:
+        os.makedirs(Filesystem.GetAbsolutePath(path), exist_ok=True)
+
+    @staticmethod
+    def IsFolderEmpty(path: str) -> None:
+        return (
+            Filesystem.DoesPathExist(path)
+            and Filesystem.IsFolder(path)
+            and os.scandir(Filesystem.GetAbsolutePath(path))
+        )
+
+    @staticmethod
     def GetAllFilepathsRecursively(root: str) -> list:
         return [
             os.path.abspath(f).replace("\\", "/")
