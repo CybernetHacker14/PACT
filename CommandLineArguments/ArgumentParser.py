8821950319803fa57d7e45ce372bfbc69965062f
Ujjwal Raut <ujjwalraut.14@gmail.com>
2022-06-02 20:39:19 +0530

    Argument parsing changes (#13)

--- CommandLineArguments/ArgumentParser.py
+++ CommandLineArguments/ArgumentParser.py
@@ -5,7 +5,9 @@ from enum import Enum
 class ArgumentType(Enum):
     VARIABLE = 1
     LIST = 2
-    FLAG = 3
+    CONST = 3
+    FLAG_TRUE = 4
+    FLAG_FALSE = 5
 
 
 class ArgumentParser:
@@ -17,9 +19,10 @@ class ArgumentParser:
     def AddArgumentParameter(
         self,
         argument: str,
-        argumentType=ArgumentType.VARIABLE,
-        argumentValueType=str,
+        argumentType: ArgumentType.VARIABLE,
+        argumentValueType: str,
         description: str = None,
+        constValue=None,
     ):
         if argumentType == ArgumentType.VARIABLE:
             self.parser.add_argument(
@@ -33,10 +36,23 @@ class ArgumentParser:
                 type=argumentValueType,
                 help=description,
             )
-        elif argumentType == ArgumentType.FLAG:
+        elif argumentType == ArgumentType.CONST:
+            self.parser.add_argument(
+                argument,
+                action="store_const",
+                type=argumentValueType,
+                const=constValue,
+                help=description,
+            )
+        elif argumentType == ArgumentType.FLAG_TRUE:
             self.parser.add_argument(argument, action="store_true", help=description)
+        elif argumentType == ArgumentType.FLAG_FALSE:
+            self.parser.add_argument(argument, action="store_false", help=description)
 
-    def ProcessArguments(self):
-        self.args = self.parser.parse_args()
+    def ProcessArguments(self, testArguments=None):
+        if not testArguments is None:
+            self.args = self.parser.parse_args(testArguments)
+        else:
+            self.args = self.parser.parse_args()
         self.config = vars(self.args)
         return self.config
